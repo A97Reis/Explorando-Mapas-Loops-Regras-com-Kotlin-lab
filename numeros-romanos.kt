6e9394c57c129a3d639efeb19e76919d6749a2a9
A97Reis <44835214+A97Reis@users.noreply.github.com>
2023-01-06 00:14:08 +0000

    Codigo final

---
+++ numeros-romanos.kt
@@ -0,0 +1,45 @@
+fun main() {
+    val numeroRomano: String? = readLine()
+    val numerosRomanos = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+
+   // TODO("Criar uma variável mutável para armazenar o $resultado")
+   var resultado: Int = 0
+    
+    for (i in numeroRomano!!.indices) {
+        //println("$resultado resultado inicio do for")
+        // Recupera o valor em romano do indice atual. 
+        val atual = numerosRomanos.getValue(numeroRomano[i])
+        // Recupera o valor em romano do próximo indice (caso exista).
+        val proximo = when (i + 1) {
+            // Caso o próximo indice não exista, atribui 0 à variável $proximo.
+            numeroRomano.length -> 0 
+            // Caso contrário, atribui o valor em romano equivalente ao próximo indice.
+            else -> numerosRomanos.getValue(numeroRomano[i + 1])
+    	}
+                    
+        //println("atual $atual")
+        //println("proximo $proximo")
+    	// TODO("Criar as condições necessárias para cálculo do $resultado"
+       
+       if (proximo == 0) {
+           break
+       } else if (proximo > atual) {
+           resultado += proximo - atual
+       } else {
+           resultado += atual
+       }
+
+        //println("$resultado resultado fim do for")
+    }
+    
+    
+    print(resultado)
+}
